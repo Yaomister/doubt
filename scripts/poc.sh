0df206012b7293eeef41e8ffe328f35224ba2d86
Eric Yao <ericyaohan@yahoo.com>
2026-09-22 20:41:59 -0400

    removed two experiments from POC

--- scripts/poc.sh
+++ scripts/poc.sh
@@ -12,9 +12,7 @@ source /home/yao.eric/doubt/.venv/bin/activate
 
 RUNS=(
   "--method baseline"
-  "--method rethink --epsilon 0"
-  "--method rethink --epsilon 0.005"
-  "--method rethink --epsilon 0.005 --random"
+  "--method rethink --epsilon 0.005" 
 )
 
 time python experiment.py --smoke-test ${RUNS[$SLURM_ARRAY_TASK_ID]}
